3b1ba05d3ba3dbc2c0548e61bcbea2b095eccf8b
mauta <anna.opareva@gmail.com>
2020-12-11 10:21:01 +0300

    init branch + fix some styles

--- src/utils/counting_cases.js
+++ src/utils/counting_cases.js
@@ -1,56 +1,56 @@
 function count(massCases, country, value, flag = '') {
   massCases.push({
     src: flag,
-    country: country,
-    count: value
+    country,
+    count: value,
   });
   return massCases;
 }
 
 export function globalCountCases(json, massCases = []) {
-  json.forEach(keys => {
+  json.forEach((keys) => {
     count(massCases, keys.country, keys.cases, keys.countryInfo.flag);
   });
   return massCases;
 }
 
 export function globalCountDeaths(json, massCases = []) {
-  json.forEach(keys => {
+  json.forEach((keys) => {
     count(massCases, keys.country, keys.deaths, keys.countryInfo.flag);
   });
   return massCases;
 }
 
 export function globalCountRecovered(json, massCases = []) {
-  json.forEach(keys => {
+  json.forEach((keys) => {
     count(massCases, keys.country, keys.recovered, keys.countryInfo.flag);
   });
   return massCases;
 }
 
 export function newCountCases(json, massCases = []) {
-  json.forEach(keys => {
+  json.forEach((keys) => {
     count(massCases, keys.country, keys.todayCases);
   });
   return massCases;
 }
 
 export function newCountDeaths(json, massCases = []) {
-  json.forEach(keys => {
+  json.forEach((keys) => {
     count(massCases, keys.country, keys.todayDeaths);
   });
   return massCases;
 }
 
 export function newCountRecovered(json, massCases = []) {
-  json.forEach(keys => {
+  json.forEach((keys) => {
     count(massCases, keys.country, keys.todayRecovered);
   });
   return massCases;
 }
 
 export function globalCountSort(massCases) {
-  massCases.sort(function (a, b) {
+  massCases.sort((a, b) => {
     if (a.count > b.count) {
       return -1;
     }
