ecea4b85cd2501a702bd26983943e3957064d3c0
mauta <anna.opareva@gmail.com>
2020-12-13 14:06:27 +0300

    feat: add search event

--- src/block/search.js
+++ src/block/search.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-new */
 import Control from '../utils/control';
+import Toggle from '../utils/toggle';
 import BtnSearch from './btn_search';
 
 export default class Search extends Control {
@@ -10,24 +12,35 @@ export default class Search extends Control {
     this.input.node.pattern = '^[a-zA-Z\s]+$';
 
     this.ul = new Control(this.node, 'ul', 'search__list search__list--hidden');
-    // new Control(this.ul.node, 'li', 'search__item', 'первый');
-    // new Control(this.ul.node, 'li', 'search__item', 'первый');
-    // new Control(this.ul.node, 'li', 'search__item', 'первый');
-    // new Control(this.ul.node, 'li', 'search__item', 'первый');
-    // new Control(this.ul.node, 'li', 'search__item', 'первый');
-
     const countries = ['Afghanistan', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bangladesh', 'Barbados', 'Bahamas', 'Bahrain', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'British Indian Ocean Territory', 'British Virgin Islands', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burma', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo-Brazzaville', 'Congo-Kinshasa', 'Cook Islands', 'Costa Rica', 'Croatia', 'Cura?ao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'East Timor', 'Ecuador', 'El Salvador', 'Egypt', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Federated States of Micronesia', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Lands', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard and McDonald Islands', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn Islands', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'R?union', 'Romania', 'Russia', 'Rwanda', 'Saint Barth?lemy', 'Saint Helena', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin', 'Saint Pierre and Miquelon', 'Saint Vincent', 'Samoa', 'San Marino', 'S?o Tom? and Pr?ncipe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia', 'South Korea', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Svalbard and Jan Mayen', 'Sweden', 'Swaziland', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican City', 'Vietnam', 'Venezuela', 'Wallis and Futuna', 'Western Sahara', 'Yemen', 'Zambia', 'Zimbabwe'].map((item) => item.toLowerCase());
     this.word = '';
-    this.input.node.addEventListener('keypress', (el) => {
+
+    this.input.node.addEventListener('keyup', (e) => {
       this.ul.node.classList.remove('search__list--hidden');
       this.ul.clear();
       this.word = this.input.node.value;
       this.arrCountry = countries.filter((item) => item.startsWith(this.word));
       this.arrCountry.forEach((item) => {
-        new Control(this.ul.node, 'li', 'search__item', `${item}`);
+        new Toggle(this.ul.node, 'li', 'search__item--active', 'search__item', `${item}`);
+        // можно дописать что мы ждем от клика в этот раз
       });
+
+      if (this.arrCountry.length === 0 && this.word.length !== 0) {
+        this.input.node.classList.add('search__input--invalid');
+      } else {
+        this.input.node.classList.remove('search__input--invalid');
+      }
+      if (this.arrCountry.length === 1) {
+        this.input.node.value = this.arrCountry[0];
+        this.ul.node.classList.add('search__list--hidden');
+      }
       if (this.arrCountry.length === 0) {
         this.ul.node.classList.add('search__list--hidden');
+      }
+
+      if (e.key === 'Enter') {
+        // вот здесь подумать какое событие мы передаем и где мы его объявляем
+        this.dispath('');
       }
     });
   }
